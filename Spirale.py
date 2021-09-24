8d63b9b2195cf2bdb9da0efae9b72384dda17726
Rémy Fayet <52427159+rems64@users.noreply.github.com>
2021-09-24 14:37:13 +0200

    Update Spirale.py

--- Spirale.py
+++ Spirale.py
@@ -1,1 +1,29 @@
+import math
+from numpy import *
 
+direction = [1,0]
+
+
+def getNext(v):
+    if v[0]==0:
+        if v[1]==1:return [[-1,0],False]
+        return [[1,0],True]
+    if v[0]==-1:return [[0,-1],False]
+    return [[0,1],False]
+
+
+def spirale(M):
+    borne1 = [0, 0]
+    borne2 = [M.shape[0]-1, M.shape[1]-1]
+    
+
+
+tableau = zeros((5,5))
+
+# print(getNext([1,0]))
+# print(getNext([0,1]))
+# print(getNext([-1,0]))
+# print(getNext([0,-1]))
+
+print(tableau)
+print(spirale(tableau))
