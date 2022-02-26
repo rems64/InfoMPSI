0d2c546b705966aabd1303a5af0f08c4272aaa41
Rémy Fayet <52427159+rems64@users.noreply.github.com>
2022-02-26 17:00:22 +0100

    Added comments on pendule2.py

--- pendule2.py
+++ pendule2.py
@@ -2,26 +2,39 @@ import scipy.integrate
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Paramètres
 g = 9.81
 l = 0.01
 
 tau = np.sqrt(l/g)
+
+# Système d'équations différentielles liées
 
 def f(t, y):
     theta = y[0]
     omega = y[1]
     return [omega, -1/tau**2*np.sin(theta)]
 
+# Solution de l'équation différentielle linéaire pour theta petit
 def linear(t, theta0):
     return theta0*np.cos(t/tau)
 
+# Conditions initiales
 begins = [np.pi/20, np.pi/6, np.pi/3, np.pi/2]
+
+# Texte des conditions initialies pour l'affichage
 labels = [r"$Condition\/initiale : \frac{\pi}{20}$", r"$Condition\/initiale : \frac{\pi}{6}$", r"$Condition\/initiale : \frac{\pi}{3}$", r"$Condition\/initiale : \frac{\pi}{2}$"]
-end = 3*2*np.pi*tau
+
+# 3 oscillations du pendule
+end = 3 * 2*np.pi*tau
+
+# Tableau des solutions approchées
 results = [scipy.integrate.solve_ivp(f, [0,end], [begin, 0], t_eval=np.linspace(0,end,1000)) for begin in begins]
 
-plt.figure(figsize=(10,10))
 
+
+# Affichage
+plt.figure(figsize=(10,10))
 
 i=0
 for r in results:
